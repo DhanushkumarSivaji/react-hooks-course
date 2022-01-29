1c8bbb4aa0d61782fa4598ea19d323fe83aab072
Dhanush kumar Sivaji <dhanushkumarsivaji@gmail.com>
2022-01-29 18:10:14 +0530

    completed UseImperativeHandle hook

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import {
 } from "react-router-dom";
 import UseState from './components/useState';
 import UseReducer from './components/useReducer';
+import UseCallback from './components/useCallback';
+import UseContext from './components/useContext';
+import UseEffect from './components/useEffect';
+import UseLayoutEffect from './components/useLayoutEffect';
+import UseMemo from './components/useMemo';
+import UseImperativeHandle from './components/useImperativeHandle';
 import './App.css';
 
 function App() {
@@ -23,12 +29,36 @@ function App() {
             <li>
               <Link to="/usereducer">usereducer</Link>
             </li>
+            <li>
+              <Link to="/usecallback">usecallback</Link>
+            </li>
+            <li>
+              <Link to="/usecontext">usecontext</Link>
+            </li>
+            <li>
+              <Link to="/useeffect">useeffect</Link>
+            </li>
+            <li>
+              <Link to="/uselayouteffect">uselayouteffect</Link>
+            </li>
+            <li>
+              <Link to="/usememo">usememo</Link>
+            </li>
+            <li>
+              <Link to="/useimperativehandle">useimperativehandle</Link>
+            </li>
           </ul>
         </nav>
     </div>
       <Routes>
           <Route exact path="/usestate" element={<UseState/>} />
           <Route exact path="/usereducer" element={<UseReducer/>} />
+          <Route exact path="/usecallback" element={<UseCallback/>} />
+          <Route exact path="/usecontext" element={<UseContext/>} />
+          <Route exact path="/useeffect" element={<UseEffect/>} />
+          <Route exact path="/uselayouteffect" element={<UseLayoutEffect/>} />
+          <Route exact path="/usememo" element={<UseMemo/>} />
+          <Route exact path="/useimperativehandle" element={<UseImperativeHandle/>} />
       </Routes>
     </Router>
     </>
