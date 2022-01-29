71e88da0aa6a224afc78eeb92d0d50bd4447907a
Dhanush kumar Sivaji <dhanushkumarsivaji@gmail.com>
2022-01-29 17:35:27 +0530

    Completed useEffect, useState, useLayoutEffect, useRef

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import UseState from './components/useState';
+import UseReducer from './components/useReducer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+    <Router>
+    <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/usestate">usestate</Link>
+            </li>
+            <li>
+              <Link to="/usereducer">usereducer</Link>
+            </li>
+          </ul>
+        </nav>
     </div>
+      <Routes>
+          <Route exact path="/usestate" element={<UseState/>} />
+          <Route exact path="/usereducer" element={<UseReducer/>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
